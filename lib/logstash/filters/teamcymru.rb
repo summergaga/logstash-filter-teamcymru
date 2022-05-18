bb30cbacbd6677fbc72537e7cbabb31f2246f1c8
summergaga <84578439+summergaga@users.noreply.github.com>
2022-05-18 17:07:38 +0300

    Update \lib\logstash\filters\teamcymru.rb
    Moved to firefox
    Moved to Full xpath
    Fixed some typos

--- lib/logstash/filters/teamcymru.rb
+++ lib/logstash/filters/teamcymru.rb
@@ -1,24 +1,25 @@
 # encoding: utf-8
 require "logstash/filters/base"
 require "watir"
-require 'webdrivers/chromedriver'
+require "headless"
+require "webdrivers/geckodriver"
 
 class LogStash::Filters::Teamcymru < LogStash::Filters::Base
   config_name "teamcymru"
   config :ip, :validate => :string, :required => true
-  config :target, :validate => :string, :default => "teamcymru"
+  config :target, :validate => :string, :default => "teamcymru_score"
   public
   def register
     # Add instance variables
   end # def register
   public
   def filter(event)
-    browser = Watir::Browser.new :chrome, headlesss: true
+    browser = Watir::Browser.new :firefox, headless: true
     browser.goto('https://reputation.team-cymru.com')
     browser.textarea(class: 'input').set(event.get(@ip))
     browser.button(id: 'search').click
-    browser.element(xpath: '//*[@id="ipCheckContainer"]/table/tbody/tr/td[2]').wait_until(&:present?)
-    answer = browser.element(xpath: '/html/body/section[1]/div/div[2]/table/tbody/tr/td[2]').texts
+    browser.element(xpath: '/html/body/section[1]/div/div[2]/table/tbody/tr/td[2]').wait_until(&:present?)
+    answer = browser.element(xpath: '/html/body/section[1]/div/div[2]/table/tbody/tr/td[2]').text
     event.set(@target, answer)
     browser.close
     # filter_matched should go in the last line of our successful code
